f5e4fe19ec7bad89de634cea4e11f90f8e7d6656
Blaked <dorian.becker@gadz.org>
2012-12-12 19:30:51 +0100

    bug fixes

--- OfflineLocal.js
+++ OfflineLocal.js
@@ -1,5 +1,4 @@
 function onload(){
-	//...
 	getData();
 }
  
@@ -8,74 +7,37 @@ function getData(){
 		for(var i=0; i<window.localStorage.length+1;i++){
 			var key = window.localStorage.key(i);
 			var value = window.localStorage.getItem(key);
-                                    displayData(value);
-
+            displayData(value);
 		}
 		
 	}
 }
  
 function saveData(){
-	for(var i=1; i<nombre_equipes+1;i++)
-		{
-		
+	for(var i=1; i<nombre_equipes+1;i++){
 		var varee ={};
-
-		
-		
 		window.localStorage.setItem(i,cnt[i]);
 		window.localStorage.setItem(i+nombre_equipes,joueurs_equipe[i]);
-
-		}
-	
-
-
-
-	
-	//var ee2 = document.getElementById("e2").innerHTML;
-	//window.localStorage.setItem(2, ee2);
-	//var ee3 = document.getElementById("e3").innerHTML;
-	//window.localStorage.setItem(3, ee3);
+	}
 }
  
 function displayData(data){
-	for(var i=1; i<window.localStorage.length+1;i++)  //window.localStorage.length
-		{
+	for(var i=1; i<window.localStorage.length+1;i++){
 		var vare = 'e'+i;
 		var vari = 'i'+i;
-
 		var varee ={};
-
 
 		cnt[i]=parseInt(window.localStorage.getItem(i));
 		document.getElementById(vare).innerHTML =cnt[i];
 		if (cnt[i] != null){vare = parseInt(cnt[i]);}
 		else{cnt[i]=0;}
-
-
 		
 		joueurs_equipe[i]=window.localStorage.getItem(i+nombre_equipes);
 		document.getElementById(vari).value =joueurs_equipe[i];
-		}
+		if (joueurs_equipe[i] != null || joueurs_equipe[i]<1){joueurs_equipe[i] = parseInt(joueurs_equipe[i]);}
+		else{joueurs_equipe[i]=1;}
+	}
 
-			
-		/*setTimeout(function() {
-  		if (i=3) {alert(keyi); };
-  		},5000);
-		
-
-		setTimeout(function() {
-  		if (i=3) {alert(joueurs_equipe[i]); };
-  		},5000);*/
-
-	//ee2=window.localStorage.getItem(2);
-	//document.getElementById("e2").innerHTML =ee2;
-	//if (ee2 != null){e2 = parseInt(ee2);}
-
-	//ee3=window.localStorage.getItem(3);
-	//document.getElementById("e3").innerHTML =ee3;
-	//if (ee3 != null){e3 = parseInt(ee3);}
-	
 }
 
 
