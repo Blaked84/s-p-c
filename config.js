b60b3c91983b52f6d9dc2f1a7699d5158d716b19
Blaked <dorian.becker@gadz.org>
2012-12-12 14:42:45 +0100

    display team name

--- config.js
+++ config.js
@@ -1,2 +1,24 @@
+var nom_equipe = {}; //ne pas toucher
+
 // mettre ici le nombre d'équipes.Attention 17 maxi
-var nombre_equipes=10;
+var nombre_equipes=6;
+
+// nom des équipes:
+nom_equipe[1]='Equipe 1';
+nom_equipe[2]='Equipe 2';
+nom_equipe[3]='Equipe 3';
+nom_equipe[4]='Equipe 4';
+nom_equipe[5]='Equipe 5';
+nom_equipe[6]='Equipe 6';
+nom_equipe[7]='Equipe 7';
+nom_equipe[8]='Equipe 8';
+nom_equipe[9]='Equipe 9';
+nom_equipe[10]='Equipe 10';
+nom_equipe[11]='Equipe 11';
+nom_equipe[12]='Equipe 12';
+nom_equipe[13]='Equipe 13';
+nom_equipe[14]='Equipe 14';
+nom_equipe[15]='Equipe 15';
+nom_equipe[16]='Equipe 16';
+nom_equipe[17]='Equipe 17';
+nom_equipe[18]='Equipe 18';
